c05f39ea466cf48537737d91468db92a7f1fdf24
cr2s367067 <cr2s367067@gmail.com>
2022-03-27 22:21:27 +0800

    add announcement function, put the bar chart in provider profile view

---
+++ FransiSRentalHouseProject/FransiSRentalHouseProject/Model/Data Models/AnnouncementDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  AnnouncementDataModel.swift
+//  FransiSRentalHouseProject
+//
+//  Created by Kuan on 2022/3/27.
+//
+
+import Foundation
---
+++ FransiSRentalHouseProject/FransiSRentalHouseProject/Model/Firestore/FirestoreFetchingAnnouncement.swift
@@ -0,0 +1,8 @@
+//
+//  FirestoreFetchingAnnouncement.swift
+//  FransiSRentalHouseProject
+//
+//  Created by Kuan on 2022/3/27.
+//
+
+import Foundation
