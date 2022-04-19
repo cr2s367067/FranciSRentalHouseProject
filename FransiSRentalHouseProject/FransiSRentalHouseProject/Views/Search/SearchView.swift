2eb6658bf04a07120f4ce57d6b001c65fc52cdbe
cr2s367067 <cr2s367067@gmail.com>
2022-04-19 15:45:04 +0800

    add sorting button to sort by tag

--- FransiSRentalHouseProject/FransiSRentalHouseProject/Views/Search/SearchView.swift
+++ FransiSRentalHouseProject/FransiSRentalHouseProject/Views/Search/SearchView.swift
@@ -8,34 +8,57 @@
 import SwiftUI
 
 struct SearchView: View {
-
-    enum Cities: String, CaseIterable {
-        case taipei = "Taipei"
-        case newTaipei = "新北市"
-        case taoyuan = "Taoyuan"
-        case taichung = "Taichung"
-        case tainan = "Tainan"
-        case kaohsiung = "Kaohsiung"
+    
+    func evaluateArray(par: Cities) -> [String] {
+        switch par {
+        case .taipei:
+            return taipeiDistrictArray
+        case .newTaipei:
+            return newTaipeiDistrictArray
+        case .taoyuan:
+            return taoyuanDistrictArray
+        case .taichung:
+            return taichungDistrictArray
+        case .tainan:
+            return tainanDistrictArray
+        case .kaohsiung:
+            return kaohsiungDistrictArray
+        case .hsinchu:
+            return hsinchuDistrictArray
+        case .miaoli:
+            return miaoliDistrictArray
+        case .chanhua:
+            return changhuaDistrictArray
+        case .nantou:
+            return nantouDistrictArray
+        case .yunlin:
+            return yunlinDistrictArray
+        case .chiayi:
+            return chiayiDistrictArray
+        case .pingtung:
+            return pingtungDistrictArray
+        case .yilan:
+            return yilanDistrictArray
+        case .hualien:
+            return hualienDistrictArray
+        case .taitung:
+            return taitungDistrictArray
+        case .penghu:
+            return penghuDistrictArray
+        case .kinmen:
+            return kinmenDistrictArray
+        case .matsu:
+            return matsuDistrictArray
+        case .keelungCity:
+            return keelungDistrictArray
+        case .hsinchuCity:
+            return hsinchuCityArray
+        case .chiayiCity:
+            return chiayiCityArray
+        }
     }
     
-    enum Counties: String, CaseIterable {
-        case hsinchu = "Hsinchu County"
-        case miaoli = "Miaoli County"
-        case chanhua = "Chanhua County"
-        case nantou = "Nantou County"
-        case yunlin = "Yunlin County"
-        case chiayi = "Chiayi County"
-        case pingtung = "Pingtung County"
-        case yilan = "Yilan County"
-        case hualien = "Hualien County"
-        case taitung = "Taitung County"
-        case penghu = "Penghu County"
-        case kinmen = "Kinmen County"
-        case matsu = "Matsu County"
-        case keelungCity = "Keelung City"
-        case hsinchuCity = "Hsinchu City"
-        case chiayiCity = "Chiayi City"
-    }
+   
     
     @EnvironmentObject var appViewModel: AppViewModel
     @EnvironmentObject var localData: LocalData
@@ -52,8 +75,33 @@ struct SearchView: View {
     
     @FocusState private var isFocused: Bool
     
+    @State private var holderArray = [String]()
+    
     let cityAarray: [String] = Cities.allCases.map({$0.rawValue})
-    let countyArray: [String] = Counties.allCases.map({$0.rawValue})
+    let taipeiDistrictArray: [String] = TaipeiDistrict.allCases.map({$0.rawValue})
+    let newTaipeiDistrictArray: [String] = NewTaipeiDistrict.allCases.map({$0.rawValue})
+    let taoyuanDistrictArray: [String] = TaoyuanDistrict.allCases.map({$0.rawValue})
+    let taichungDistrictArray: [String] = TaichungDistrict.allCases.map({$0.rawValue})
+    let tainanDistrictArray: [String] = TainanDistrict.allCases.map({$0.rawValue})
+    let kaohsiungDistrictArray: [String] = KaohsiungDistrict.allCases.map({$0.rawValue})
+    let yilanDistrictArray: [String] = YilanDistrict.allCases.map({$0.rawValue})
+    let nantouDistrictArray: [String] = NantouDistrict.allCases.map({$0.rawValue})
+    let hsinchuDistrictArray: [String] = HsinchuDistrict.allCases.map({$0.rawValue})
+    let miaoliDistrictArray: [String] = MiaoliDistrict.allCases.map({$0.rawValue})
+    let changhuaDistrictArray: [String] = ChanghuaDistrict.allCases.map({$0.rawValue})
+    let yunlinDistrictArray: [String] = YunlinDistrict.allCases.map({$0.rawValue})
+    let chiayiDistrictArray: [String] = ChiayiDistrict.allCases.map({$0.rawValue})
+    let pingtungDistrictArray: [String] = PingtungDistrict.allCases.map({$0.rawValue})
+    let taitungDistrictArray: [String] = TaitungDistrict.allCases.map({$0.rawValue})
+    let hualienDistrictArray: [String] = HualienDistrict.allCases.map({$0.rawValue})
+    let penghuDistrictArray: [String] = PenghuDistrict.allCases.map({$0.rawValue})
+    let keelungDistrictArray: [String] = KeelungDistrict.allCases.map({$0.rawValue})
+    let hsinchuCityArray: [String] = HsinchuCity.allCases.map({$0.rawValue})
+    let chiayiCityArray: [String] = ChiayiCity.allCases.map({$0.rawValue})
+    let matsuDistrictArray: [String] = MatsuDistrict.allCases.map({$0.rawValue})
+    let kinmenDistrictArray: [String] = KinmenDistrict.allCases.map({$0.rawValue})
+    
+    
     
     let uiScreenWidth = UIScreen.main.bounds.width
     let uiScreenHeight = UIScreen.main.bounds.height
@@ -65,6 +113,12 @@ struct SearchView: View {
                     .fill(LinearGradient(gradient: Gradient(colors: [Color("background1"), Color("background2")]), startPoint: .top, endPoint: .bottom))
                     .edgesIgnoringSafeArea([.bottom, .top])
                 VStack(spacing: 10) {
+                    Button {
+                        holderArray = evaluateArray(par: .taipei)
+                        print(holderArray)
+                    } label: {
+                        Text("test")
+                    }
                     Spacer()
                     //: Search TextField For Temp
                     HStack {
@@ -124,20 +178,7 @@ struct SearchView: View {
                                 .frame(width: 25, height: 26)
                         }
                     }
-                    if showTags {
-                        HStack {
-                            ScrollView(.horizontal, showsIndicators: false) {
-                                HStack {
-                                    ForEach(cityAarray, id: \.self) { city in
-                                        sortingTagUnit(name: city)
-                                            .onTapGesture {
-                                                searchName = tagCollecte(firstTag: city)
-                                            }
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    showTagView()
                     //: Scroll View
                     VStack {
                         identityRoomsProducts(showRooms: showRooms, showProducts: showProducts)
@@ -165,6 +206,50 @@ struct SearchView: View {
 //}
 
 extension SearchView {
+    
+    @ViewBuilder
+    func showTagView() -> some View {
+        if showTags {
+            VStack {
+                HStack {
+                    Text("City&County")
+                        .foregroundColor(.white)
+                        .font(.system(size: 15))
+                    Spacer()
+                }
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(cityAarray, id: \.self) { city in
+                            sortingTagUnit(name: city)
+                                .onTapGesture {
+                                    searchName = tagCollecte(firstTag: city)
+                                    let defaultValue = Cities.taipei
+                                    holderArray = evaluateArray(par: Cities(rawValue: city) ?? defaultValue)
+                                }
+                        }
+                    }
+                }
+                if !holderArray.isEmpty {
+                    HStack {
+                        Text("District")
+                            .foregroundColor(.white)
+                            .font(.system(size: 15))
+                        Spacer()
+                    }
+                    ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(holderArray, id: \.self) { city in
+                            sortingTagUnit(name: city)
+                                .onTapGesture {
+                                    searchName = tagCollecte(firstTag: city)
+                                }
+                        }
+                    }
+                }
+                }
+            }
+        }
+    }
     
     func address(input: RoomInfoDataModel) -> String {
         let zipCode = input.zipCode
@@ -204,6 +289,12 @@ extension SearchView {
                                        roomCity: result.city,
                                        roomPrice: Int(result.rentalPrice) ?? 0)
                 }
+                .simultaneousGesture(
+                    TapGesture().onEnded({ _ in
+                        searchName = ""
+                        showTags = false
+                    })
+                )
             }
         }
     }
@@ -281,10 +372,10 @@ extension SearchView {
     }
     
     private func charCount(name: String) -> CGFloat {
-        return CGFloat(Double(name.count) * 2.3)
+        return CGFloat(Double(name.count) * 5)
     }
     
-    private func tagCollecte(firstTag: String?, secTag: String? = nil) -> String {
+    private func tagCollecte(firstTag: String? = nil, secTag: String? = nil) -> String {
         return (firstTag ?? "") + (secTag ?? "")
     }
 }
